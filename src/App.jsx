c45b036ee52be493209a6e1ee476d7762c3507ce
ifer-itcast <yangkangkang@itcast.cn>
2020-08-03 17:19:57 +0800

    按需加载antd-mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 
+import { Button } from "antd-mobile";
+
 class App extends Component {
     render() {
-        return <div>App</div>;
+        return <div>
+            <Button type="primary">primary</Button>
+        </div>;
     }
 }
 
