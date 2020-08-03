25ec9c5576e4064246f3f724700e8d030f133616
ifer-itcast <yangkangkang@itcast.cn>
2020-08-03 23:55:33 +0800

    获取轮播图数据

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,46 +1,45 @@
 import React, { Component } from "react";
 import { Carousel } from "antd-mobile";
+import axios from "axios";
 
 export default class Index extends Component {
     state = {
-        data: ["1", "2", "3"],
+        swipers: [],
     };
+    async getSwipers() {
+        const { data } = await axios.get("http://localhost:8080/home/swiper");
+        this.setState({ swipers: data.body });
+    }
     componentDidMount() {
-        // simulate img loading
-        setTimeout(() => {
-            this.setState({
-                data: [
-                    "AiyWuByWklrrUDlFignR",
-                    "TekJlZRVCjLFexlOCuWn",
-                    "IJOtIlfsYdTyaDTRVrLI",
-                ],
-            });
-        }, 100);
+        this.getSwipers();
+    }
+    renderSwipers() {
+        return this.state.swipers.map(item =>
+            <a
+                key={item.id}
+                href="http://itcast.cn"
+                style={{
+                    display: "inline-block",
+                    width: "100%",
+                    height: 212,
+                }}
+            >
+                <img
+                    src={`http://localhost:8080${item.imgSrc}`}
+                    alt=""
+                    style={{
+                        width: "100%",
+                        verticalAlign: "top",
+                    }}
+                />
+            </a>
+        );
     }
     render() {
         return (
             <div className="index">
                 <Carousel autoplay infinite>
-                    {this.state.data.map(val =>
-                        <a
-                            key={val}
-                            href="http://www.alipay.com"
-                            style={{
-                                display: "inline-block",
-                                width: "100%",
-                                height: 212,
-                            }}
-                        >
-                            <img
-                                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                                alt=""
-                                style={{
-                                    width: "100%",
-                                    verticalAlign: "top",
-                                }}
-                            />
-                        </a>
-                    )}
+                    {this.renderSwipers()}
                 </Carousel>
             </div>
         );
