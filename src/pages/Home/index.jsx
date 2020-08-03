e0d3668f7def9309a1bc0b80f00db5d0b422519a
ifer-itcast <yangkangkang@itcast.cn>
2020-08-03 19:38:20 +0800

    调整TabBar外观和样式

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,84 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom';
-import News from '../News';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { TabBar } from "antd-mobile";
+
+import "./index.css";
 export default class Home extends Component {
+    state = {
+        selectedTab: "redTab"
+    };
+    renderContent(pageText) {
+        return <div>xxx</div>;
+    }
     render() {
         return (
-            <div>
-                Home
-                <Route path="/home/news" component={News}/>
+            <div className="home">
+                {/* TabBar */}
+                <TabBar
+                    tintColor="#21b97a"
+                    barTintColor="white"
+                    hidden={this.state.hidden}
+                >
+                    <TabBar.Item
+                        title="首页"
+                        key="Life"
+                        icon={<i className="iconfont icon-ind" />}
+                        selectedIcon={<i className="iconfont icon-ind" />}
+                        selected={this.state.selectedTab === "blueTab"}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: "blueTab",
+                            });
+                        }}
+                        data-seed="logId"
+                    >
+                        {this.renderContent("Life")}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={<i className="iconfont icon-findHouse" />}
+                        selectedIcon={<i className="iconfont icon-findHouse" />}
+                        title="找房"
+                        key="Koubei"
+                        selected={this.state.selectedTab === "redTab"}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: "redTab",
+                            });
+                        }}
+                        data-seed="logId1"
+                    >
+                        {this.renderContent("Koubei")}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={<i className="iconfont icon-infom" />}
+                        selectedIcon={<i className="iconfont icon-infom" />}
+                        title="资讯"
+                        key="Friend"
+                        selected={this.state.selectedTab === "greenTab"}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: "greenTab",
+                            });
+                        }}
+                    >
+                        {this.renderContent("Friend")}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={<i className="iconfont icon-my" />}
+                        selectedIcon={<i className="iconfont icon-my" />}
+                        title="我的"
+                        key="my"
+                        selected={this.state.selectedTab === "yellowTab"}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: "yellowTab",
+                            });
+                        }}
+                    >
+                        {this.renderContent("My")}
+                    </TabBar.Item>
+                </TabBar>
             </div>
-        )
+        );
     }
 }
