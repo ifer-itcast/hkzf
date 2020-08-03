1fc476f45ab9db603130d0dac47baf662edef48a
ifer-itcast <yangkangkang@itcast.cn>
2020-08-03 17:31:37 +0800

    配置基础路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import { Button } from "antd-mobile";
+import Home from './pages/Home';
+import CityList from './pages/CityList';
 
 class App extends Component {
     render() {
-        return <div>
-            <Button type="primary">primary</Button>
-        </div>;
+        return (
+            <Router>
+                <div className="app">
+                    <ul>
+                        <li>
+                            <Link to="/home">首页</Link>
+                        </li>
+                        <li>
+                            <Link to="/citylist">城市</Link>
+                        </li>
+                    </ul>
+                    <Route path="/home" component={Home}/>
+                    <Route path="/citylist" component={CityList}/>
+                </div>
+            </Router>
+        );
     }
 }
 
