9a9562fa3449ae0b0519cf3ccc0476d65abf1667
ifer-itcast <yangkangkang@itcast.cn>
2020-08-04 11:59:02 +0800

    获取并处理城市列表数据

--- src/pages/CityList/index.jsx
+++ src/pages/CityList/index.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
-import { NavBar } from 'antd-mobile';
+import { NavBar } from "antd-mobile";
+import axios from "axios";
 
 import "./index.scss";
 
+const formatCityData = list => {
+    const cityList = {};
+    list.forEach(item => {
+        const first = item.short.substr(0, 1);
+        if (cityList[first]) {
+            cityList[first].push(item);
+        } else {
+            cityList[first] = [item];
+        }
+    });
+    const cityIndex = Object.keys(cityList).sort();
+    return { cityList, cityIndex };
+};
+
 export default class CityList extends Component {
+    componentDidMount() {
+        // 获取城市列表数据
+        this.getCityList();
+    }
+    async getCityList() {
+        const { data } = await axios.get(
+            "http://localhost:8080/area/city?level=1"
+        );
+        const { cityList, cityIndex } = formatCityData(data.body);
+        console.log(cityList, cityIndex);
+    }
     render() {
         return (
             <div className="citylist">
