f8a0fc6e46a283511d122877f09057375f2548d7
ifer-itcast <yangkangkang@itcast.cn>
2020-08-04 11:32:21 +0800

    CityList组件NavBar结构和样式

--- src/pages/CityList/index.jsx
+++ src/pages/CityList/index.jsx
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { NavBar } from 'antd-mobile';
+
+import "./index.scss";
 
 export default class CityList extends Component {
     render() {
         return (
-            <div>
-                CityList
+            <div className="citylist">
+                <NavBar
+                    className="navbar"
+                    mode="light"
+                    icon={<i className="iconfont icon-back" />}
+                    onLeftClick={() => this.props.history.go(-1)}
+                >
+                    城市选择
+                </NavBar>
             </div>
-        )
+        );
     }
 }
