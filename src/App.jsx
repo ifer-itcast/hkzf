435347a896a61d446f6ab7a4b107f24825e0c0c7
ifer-itcast <yangkangkang@itcast.cn>
2020-08-04 10:50:35 +0800

    展示百度地图

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-import Home from './pages/Home';
-import CityList from './pages/CityList';
+import Home from "./pages/Home";
+import CityList from "./pages/CityList";
+import Map from "./pages/Map";
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className="app">
-                    <Route path="/" exact render={() => <Redirect to="/home"/>}/>
-                    <Route path="/home" component={Home}/>
-                    <Route path="/citylist" component={CityList}/>
+                    <Route
+                        path="/"
+                        exact
+                        render={() => <Redirect to="/home" />}
+                    />
+                    <Route path="/home" component={Home} />
+                    <Route path="/citylist" component={CityList} />
+                    <Route path="/map" component={Map} />
                 </div>
             </Router>
         );
---
+++ src/pages/Map/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./index.scss";
+
+export default class Map extends Component {
+    componentDidMount() {
+        const map = new window.BMap.Map("container");
+        const point = new window.BMap.Point(116.404, 39.915);
+        map.centerAndZoom(point, 15);
+    }
+    render() {
+        return (
+            <div className="map">
+                <div id="container" />
+            </div>
+        );
+    }
+}
