28252f06e8da9165102d47778baaa46c15e813b6
ifer-itcast <yangkangkang@itcast.cn>
2020-08-04 18:03:18 +0800

    根据定位展示当前城市并添加控件

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
-import NavHeader from '../../components/NavHeader';
+import NavHeader from "../../components/NavHeader";
 
 // import "./index.scss";
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
+const BMap = window.BMap;
 export default class Map extends Component {
     componentDidMount() {
-        const map = new window.BMap.Map("container");
-        const point = new window.BMap.Point(116.404, 39.915);
-        map.centerAndZoom(point, 15);
+        this.initMap();
     }
+    initMap = () => {
+        const { label, value } = JSON.parse(localStorage.getItem("hkzf_city"));
+
+        const map = new window.BMap.Map("container");
+
+        // const point = new window.BMap.Point(116.404, 39.915);
+        // map.centerAndZoom(point, 15);
+
+        const myGeo = new window.BMap.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野
+        myGeo.getPoint(
+            label,
+            point => {
+                if (point) {
+                    // 1. 初始化地图
+                    map.centerAndZoom(point, 11);
+                    // 2. 添加控件
+                    map.addControl(new BMap.NavigationControl()); // 放大缩小
+                    map.addControl(new BMap.ScaleControl()); // 比例尺
+                }
+            },
+            label
+        );
+    };
     render() {
         return (
             <div className={styles.map}>
