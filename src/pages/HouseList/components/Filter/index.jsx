8a77bd2edeaf2667bcd63ff9cec47dec2db7a14b
ifer-itcast <yangkangkang@itcast.cn>
2020-08-08 22:04:01 +0800

    优化遮罩层的动画效果

--- src/pages/HouseList/components/Filter/index.jsx
+++ src/pages/HouseList/components/Filter/index.jsx
@@ -234,13 +234,46 @@ export default class Filter extends Component {
             />
         );
     };
+    renderMask = () => {
+        const { openType } = this.state;
+        /* if (openType === "more" || openType === "") {
+            return null;
+        }
+        return (
+            <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+                {props =>
+                    <div
+                        style={props}
+                        className={styles.mask}
+                        onClick={() => this.onCancel(openType)}
+                    />}
+            </Spring>
+        ); */
+        const isHide = openType === "more" || openType === "";
+        // 保证了 Spring 是一直都渲染的
+        return (
+            <Spring from={{ opacity: 0 }} to={{ opacity: isHide ? 0 : 1 }}>
+                {props => {
+                    if (props.opacity === 0) {
+                        return null;
+                    }
+                    return (
+                        <div
+                            style={props}
+                            className={styles.mask}
+                            onClick={() => this.onCancel(openType)}
+                        />
+                    );
+                }}
+            </Spring>
+        );
+    };
     render() {
         const { titleSelectedStatus, openType } = this.state;
-
         return (
             <div className={styles.root}>
                 {/* 前三个菜单的遮罩层 */}
-                {openType === "area" ||
+                {/* {openType === "area" ||
                 openType === "mode" ||
                 openType === "price"
                     ? <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
@@ -251,7 +284,8 @@ export default class Filter extends Component {
                                   onClick={() => this.onCancel(openType)}
                               />}
                       </Spring>
-                    : null}
+                    : null} */}
+                {this.renderMask()}
                 <div className={styles.content}>
                     {/* 标题栏 */}
                     <FilterTitle
