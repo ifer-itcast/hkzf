343ecd818baa20fcc2a31dcacc313f9380f04e4e
ifer-itcast <yangkangkang@itcast.cn>
2020-08-08 22:16:14 +0800

    准备房屋详情页面模板

--- src/components/NavHeader/index.jsx
+++ src/components/NavHeader/index.jsx
@@ -4,16 +4,23 @@ import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // import "./index.scss";
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
-function NavHeader({ children, history, onLeftClick }) {
+function NavHeader({
+    children,
+    history,
+    onLeftClick,
+    className,
+    rightContent,
+}) {
     const defaultHandler = () => history.go(-1);
     return (
         <NavBar
-            className={styles.navbar}
+            className={[styles.navbar, className || ""].join(" ")}
             mode="light"
             icon={<i className="iconfont icon-back" />}
             onLeftClick={onLeftClick || defaultHandler}
+            rightContent={rightContent}
         >
             {children}
         </NavBar>
